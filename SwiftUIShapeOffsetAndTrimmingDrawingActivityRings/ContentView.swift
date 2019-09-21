a386469c8f513944e3008f844011791d685d3359
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-09-21 19:27:10 +0300

    init

--- SwiftUIShapeOffsetAndTrimmingDrawingActivityRings/ContentView.swift
+++ SwiftUIShapeOffsetAndTrimmingDrawingActivityRings/ContentView.swift
@@ -10,7 +10,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        VStack(spacing: 20) {
+            
+            Text("Today, 9 July 2019")
+                .font(.largeTitle)
+                .padding()
+            
+            Divider()
+            
+            ZStack {
+                
+                Circle()
+                    .trim(from: 0.5, to: 1.0)
+                    .stroke(Color.pink, lineWidth: 30)
+                    .frame(width: 300, height: 300)
+                    .rotationEffect(.degrees(90))
+                
+                Circle()
+                    .trim(from: 0.75, to: 1.0)
+                    .stroke(Color.green, lineWidth: 30)
+                    .frame(width: 238, height: 238)
+                    
+                Circle()
+                    .trim(from: 0.25, to: 1.0)
+                    .stroke(Color.blue, lineWidth: 30)
+                    .frame(width: 176, height: 176)
+                    .rotationEffect(.degrees(-180))
+                    
+            }.offset(x: -30, y: 30)
+        }
     }
 }
 
